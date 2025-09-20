36b6981d396ab5dc5308327e50d8c7981ea36a68
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2025-09-20 10:30:41 +0200

    Set version

--- src/pydolce/__init__.py
+++ src/pydolce/__init__.py
@@ -5,7 +5,7 @@ from pydolce.commands.check import check
 from pydolce.commands.restyle import restyle
 from pydolce.commands.suggest import suggest
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
